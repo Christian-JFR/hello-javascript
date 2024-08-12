51acd52be14e67283e676a0c169b3af9b341f16d
Christian <christianjfr@gmail.com>
2024-08-12 16:27:52 -0500

    Sol. Ejercicios Clase 5 - Objects - Destructuring - Spread Operator

--- Basic/19-objects-exercises.js
+++ Basic/19-objects-exercises.js
@@ -5,21 +5,65 @@ https://www.twitch.tv/videos/2218300512?t=00h17m10s
 */
 
 // 1. Crea un objeto con 3 propiedades
+let theBatman = {
+	name: "Bruce Wayne",
+	"alter ego": "Batman",
+	hero: true,
+};
 
 // 2. Accede y muestra su valor
+console.table(theBatman);
+console.log(theBatman.name);
+console.log(theBatman.hero);
+console.log(theBatman["alter ego"]);
 
 // 3. Agrega una nueva propiedad
+theBatman.appearance = "1939";
+console.table(theBatman);
+console.log(theBatman.appearance);
 
 // 4. Elimina una de las 3 primeras propiedades
+delete theBatman.hero;
+console.table(theBatman);
 
 // 5. Agrega una función e invócala
+theBatman.callBatmobile = function () {
+	console.log("Batmobile is on its way");
+};
+
+console.table(theBatman);
+theBatman.callBatmobile();
 
 // 6. Itera las propiedades del objeto
+for (let key in theBatman) {
+	console.log(`${key} => ${theBatman[key]}`);
+}
 
 // 7. Crea un objeto anidado
 
-// 8. Accede y muestra el valor de las propiedades anidadas
+let characters = {
+	theBatman: {
+		name: "Bruce Wayne",
+		"alter ego": "Batman",
+		hero: true,
+		appearance: "1939",
+		callBatmobile: function () {
+			console.log("Batmobile is on its way");
+		},
+	},
+};
+console.table(characters);
 
+// 8. Accede y muestra el valor de las propiedades anidadas
+console.log(
+	`${characters.theBatman.name}\n${characters.theBatman.hero}\n${characters.theBatman.appearance}\n${characters.theBatman["alter ego"]}\n${characters.theBatman.callBatmobile}`
+);
 // 9. Comprueba si los dos objetos creados son iguales
 
+console.log(theBatman == characters);
+console.log(theBatman === characters);
+
 // 10. Comprueba si dos propiedades diferentes son iguales
+
+console.log(theBatman.name == theBatman.appearance);
+console.log(theBatman.name === theBatman.appearance);
--- Basic/21-destructuring-spreading-exercises.js
+++ Basic/21-destructuring-spreading-exercises.js
@@ -4,23 +4,86 @@ Objetos, desestructuración, propagación y clases
 https://www.twitch.tv/videos/2218300512?t=00h17m10s
 */
 
-// 1. Usa desestructuración para extraer los dos primeros elementos de un array 
+// 1. Usa desestructuración para extraer los dos primeros elementos de un array
+let colors = ["red", "blue", "pink", "green"];
+let [color1, color2] = colors;
+
+console.log(color1, color2);
 
 // 2. Usa desestructuración en un array y asigna un valor predeterminado a una variable
+let [, , , , color5 = "purple"] = colors;
+console.log(color1, color2, color5);
 
 // 3. Usa desestructuración para extraer dos propiedades de un objeto
+const enemy = {
+	name: "Goblin",
+	hp: 50,
+	power: 15,
+	isBoss: false,
+	loot: {
+		normal: ["gold", "potion"],
+		better: ["gemstone", "nail"],
+	},
+};
+
+let { hp, power } = enemy;
+console.table(enemy);
+console.log(hp, power);
 
 // 4. Usa desestructuración para extraer dos propiedades de un objeto y asígnalas
 //    a nuevas variables con nombres diferentes
 
+let { hp: health, power: damage } = enemy;
+console.log(health, damage);
+
 // 5. Usa desestructuración para extraer dos propiedades de un objeto anidado
+let {
+	loot: { normal: common },
+	loot: { better: rare },
+} = enemy;
+
+console.log(common, rare);
 
 // 6. Usa propagación para combinar dos arrays en uno nuevo
 
+const colorsA = ["yellow", "blue", "red"];
+const colorsB = ["green", "orange", "purple"];
+
+const colorsC = [...colorsA, ...colorsB];
+console.log(colorsC);
+
 // 7. Usa propagación para crear una copia de un array
 
+const colorsCCopy = [...colorsC];
+console.log(colorsCCopy);
+
 // 8. Usa propagación para combinar dos objetos en uno nuevo
+const enemy2 = {
+	name: "Dragon",
+	health: 50,
+	damage: 15,
+	isBoss: true,
+};
+
+const loot2 = {
+	normal: ["gold", "potion"],
+	better: ["gemstone", "sword"],
+};
+
+const dragon2 = { ...enemy2, ...loot2 };
+console.table(dragon2);
 
 // 9. Usa propagación para crear una copia de un objeto
+const goblin = { ...enemy };
+console.table(goblin);
 
 // 10. Combina desestructuración y propagación
+const colorsX = ["yellow", "blue", "red"];
+const colorsY = ["green", "orange", "purple"];
+
+const [colorsX1, colorsX2, colorsX3, colorsY1, colorsY2, colorsY3] = [
+	...colorsX,
+	...colorsY,
+];
+
+console.log(colorsX1, colorsX2, colorsX3, colorsY1, colorsY2, colorsY3);
